4229a8c97d57d23e48bf83e75bdad0e1dab2ee99
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2025-08-29 18:59:51 +0530

    Created About and Footer Section and Added Smooth Scroll and gsap

---
+++ script.js
@@ -0,0 +1,14 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+function circleMouseFollower() {
+  window.addEventListener("mousemove", function (dets) {
+    // console.log(dets.clientX, dets.clientY);
+    document.querySelector(
+      "#cursorcircle"
+    ).style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+  });
+}
+circleMouseFollower();
